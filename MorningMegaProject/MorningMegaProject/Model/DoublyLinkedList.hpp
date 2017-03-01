75a777d744a7127c4e333c01098d7cc0a86d6bfc
kmart22 <kmar7070@740s-pl-111.local>
2017-03-01 09:45:52 -0700

    created header files for Stack.hpp and DoublyLinkedList.hpp

---
+++ MorningMegaProject/MorningMegaProject/Model/DoublyLinkedList.hpp
@@ -0,0 +1,56 @@
+//
+//  DoublyLinkedList.hpp
+//  MorningMegaProject
+//
+//  Created by Martinsen, Kaden on 3/1/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+private:
+    BiDirectionalNode<Type> * front;
+    BiDirectionalNode<Type> * end;
+    int size;
+public:
+    virtual void add(Type value) =0;
+    virtual Type remove(int index) =0;
+    DoublyLinkedList();
+    virtual ~DoublyLinkedList() =0;
+    
+    int getSize() const;
+    BiDirectionalNode<Type> * getFront() const;
+    BiDirectionalNode<Type> * getEnd() const;
+};
+
+template <class Type>
+DoublyLinkedList<Type> :: DoublyLinkedList()
+{
+    this->size = 0;
+    this->front = nullptr;
+    this->end = nullptr;
+}
+
+template <class Type.
+DoublyLinkedList<Type> :: getSize()
+{
+    return this-size;
+}
+
+template <class Type>
+DoublyLinkedList<Type> * DoublyLinkedList :: getFront()
+{
+    
+}
+
+
+
+
+#endif /* DoublyLinkedList_h */
+
---
+++ MorningMegaProject/MorningMegaProject/Model/Stack.hpp
@@ -0,0 +1,34 @@
+//
+//  Stack.hpp
+//  MorningMegaProject
+//
+//  Created by Martinsen, Kaden on 3/1/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+template <class Type>
+class Stack : DoublyLinkedList<Type>
+{
+private:
+    
+public:
+    stack();
+    void add(Type value);
+};
+
+/*
+ the add method onlt adds to the end on a stack. never at an index
+ 
+*/
+
+
+template <class Type>
+void Stack<Type> :: add(Type valueToAdd) : DoublyLinkedList<Type> :: add(valueToAdd)
+{
+    
+}
+
+#endif /* Stack_h */
